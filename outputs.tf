1d57a6b772e81c535c3397be23101a7ff2e461c0
Adeel Ahmad <adeel@hashicorp.com>
2020-12-22 13:21:33 +0000

    added workspace name as output

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "workspace_id" {
   value       = tfe_workspace.default.id
 }
 
+output "workspace_name" {
+  description = "TFC workspace name"
+  value       = tfe_workspace.default.name
+}
+
 output "tfc_token" {
   description = "TFC token to be used by GH Actions"
   value       = tfe_team_token.default.token
